4177d2964a8a3ee9a18ffa9d7f181c4a27344af3
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-13 16:38:46 +0300

    Imported all the models and exported them from the index.ts close #12

---
+++ server/models/index.ts
@@ -0,0 +1,37 @@
+import Announcements from './announcements';
+import Assignments from './assignments';
+import AssignmentStudent from './assignmentStudent';
+import Classes from './classes';
+import ClassStudent from './classStudent';
+import Feedbacks from './feedbacks';
+import Health from './health';
+import Parents from './parents';
+import Questions from './questions';
+import Recommendations from './recommendations';
+import Reports from './reports';
+import Schedules from './schedules';
+import Students from './students';
+import Teachers from './teachers';
+import Tests from './tests';
+import TestStudent from './testStudent';
+import Users from './users';
+
+export {
+  Announcements,
+  AssignmentStudent,
+  Assignments,
+  ClassStudent,
+  Classes,
+  Feedbacks,
+  Health,
+  Parents,
+  Questions,
+  Recommendations,
+  Reports,
+  Schedules,
+  Students,
+  Teachers,
+  Tests,
+  TestStudent,
+  Users,
+};
