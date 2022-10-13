1e61c591cef0a50003af742f629ca7ec2a3efe77
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-13 21:17:44 +0300

    Added the user authentication function which will pass the user information to the next middleware close #6

--- server/middlewares/userAuth.ts
+++ server/middlewares/userAuth.ts
@@ -1,6 +1,29 @@
-// import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { verify, Secret } from 'jsonwebtoken';
 
-// const userAuth = (req: Request, res: Response, next: NextFunction) => {
-//   const token: string = req.coo;
+import { secretKey } from '../config/environment';
+import CustomError from '../utils/customError';
 
-// }
+interface CustomRequest extends Request {
+  user: object | unknown;
+}
+
+const userAuth = (req: CustomRequest, res: Response, next: NextFunction) => {
+  const token: string = req.cookies;
+
+  if (!token) {
+    throw new CustomError(401, 'Unauthenticated!'); // ? Token is invalid.
+  }
+
+  verify(token, secretKey as Secret, (err, decoded) => {
+    if (err) {
+      throw new CustomError(401, 'Unauthenticated!'); // ? Token is invalid.
+    }
+
+    req.user = decoded;
+
+    next();
+  });
+};
+
+export default userAuth;
