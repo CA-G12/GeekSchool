6adcbd69d7fd2adb67e558b0a1c4e362d954b5f7
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-13 14:50:12 +0300

    Added the initial code close #6

---
+++ server/middlewares/userAuth.ts
@@ -0,0 +1,6 @@
+// import { Request, Response, NextFunction } from 'express';
+
+// const userAuth = (req: Request, res: Response, next: NextFunction) => {
+//   const token: string = req.coo;
+
+// }
