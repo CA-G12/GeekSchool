809f07afa8964afcbc68ee8bf3fc7bc06db9f234
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-13 13:45:11 +0300

    created health sequelize thats includes id, student_id, denatl, vision, blood_pressure, cancer, diabetes, chronic, and other column. relates #12

---
+++ server/models/health.ts
@@ -0,0 +1,45 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize('');
+
+const health = sequelize.define('health', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  student_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  dental: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  vision: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  blood_pressure: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  cancer: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  diabetes: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  chronic: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  other: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+export default health;
