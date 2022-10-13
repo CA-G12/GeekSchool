5b5451cc1a7ca15485c241d41921275c1dc6373f
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-13 10:28:29 +0300

    created express app thats includes morgan, cookie parser, compression, join, and express. relates #4

---
+++ server/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import { join } from 'path';
+
+require('dotenv').config();
+
+const router = require('./routes');
+
+const app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.disable('x-powered-by');
+
+app.use('/api/v1/', router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+app.use(express.static(join(__dirname, '..', 'client', 'public', 'index.html')));
+
+export default app;
