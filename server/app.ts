11cba5d5a0e07ddba1a7260495ef0c4167ad6384
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-13 13:01:25 +0300

    update the models name related to #12

---
+++ server/app.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import { join } from 'path';
+import router from './routes';
+import { nodeEnv } from './config/environment';
+
+const app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.disable('x-powered-by');
+
+app.use('/api/v1/', router);
+
+if (nodeEnv === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+export default app;
