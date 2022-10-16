2d860c09392cae9c5def4c42e4faf840cf4336c6
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-16 15:51:02 +0300

    sync all  models related to #21

--- server/database/build.ts
+++ server/database/build.ts
@@ -2,6 +2,7 @@ import { sequelize } from '../models';
 
 const buildModels = async () => {
   await sequelize.sync({ force: true });
+  // eslint-disable-next-line no-console
   console.log('All models were synchronized successfully.');
 };
 
