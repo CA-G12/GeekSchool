7b5b5317d5228a872604ebddc89e1618a827c6e5
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-16 16:00:38 +0300

    build and seed db models related to #21

---
+++ server/database/seed.ts
@@ -0,0 +1,40 @@
+// import buildModels from './build';
+import {
+  Announcement, Assignment, AssignmentStudent, Class, ClassStudent, Feedback, Health, Parent,
+  Question, Recommendation, Report, Schedule, sequelize, Student, Teacher, Test, TestStudent, User,
+} from '../models';
+
+import {
+  announcements, assignments, assignmentsStudent, classes, classStudent, feedbacks, health,
+  parents, questions, recommendations, reports, schedules, students, teachers, tests,
+  testStudent, users,
+} from './seed/';
+
+const buildSeed = async () => {
+  // buildModels();
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(users, { validate: true });
+  await Teacher.bulkCreate(teachers, { validate: true });
+  await Parent.bulkCreate(parents, { validate: true });
+  await Student.bulkCreate(students, { validate: true });
+  await Health.bulkCreate(health, { validate: true });
+  await Class.bulkCreate(classes, { validate: true });
+  await Test.bulkCreate(tests, { validate: true });
+  await Feedback.bulkCreate(feedbacks, { validate: true });
+  await Announcement.bulkCreate(announcements, { validate: true });
+  await Assignment.bulkCreate(assignments, { validate: true });
+  await AssignmentStudent.bulkCreate(assignmentsStudent, { validate: true });
+  await ClassStudent.bulkCreate(classStudent, { validate: true });
+  await Question.bulkCreate(questions, { validate: true });
+  await Recommendation.bulkCreate(recommendations, { validate: true });
+  await Report.bulkCreate(reports, { validate: true });
+  await Schedule.bulkCreate(schedules, { validate: true });
+  await TestStudent.bulkCreate(testStudent, { validate: true });
+  // eslint-disable-next-line no-console
+  console.log('All data added');
+};
+
+buildSeed();
+
+export default buildSeed;
