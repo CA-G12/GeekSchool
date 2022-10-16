ced5a1c0500b036396f433cec022271e764b725a
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-16 12:16:34 +0300

    Renamed the singup interface to be the userRequestInterface close#7

--- server/utils/interfaces/index.ts
+++ server/utils/interfaces/index.ts
@@ -1,5 +1,5 @@
 import CustomRequest from './requestInterface';
-import UserInterface from './signupRequestInterface';
+import UserInterface from './userRequestInterface';
 import PayloadInterface from './payloadInterface';
 import UserValInterface from './userValInterface';
 
