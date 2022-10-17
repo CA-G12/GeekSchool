aad486ee0ea1faaa7822e41dc10af36085feab3f
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-17 14:50:00 +0300

    created login router endpoint. relates #49

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,9 +1,10 @@
-import { logout, userData } from '../controllers';
+import { logout, userData, login } from '../controllers';
 import { userAuth } from '../middlewares';
 
 const authRouter = require('express').Router();
 
 authRouter.post('/auth/logout', logout);
 authRouter.get('/auth/', userAuth, userData);
+authRouter.post('/login', login);
 
 export default authRouter;
