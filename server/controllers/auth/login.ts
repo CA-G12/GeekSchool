993427caabbdbbe27468f65379479d8788f3f24f
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-18 15:28:15 +0300

    added generate token promise to login controller. relates #49

--- server/controllers/auth/login.ts
+++ server/controllers/auth/login.ts
@@ -1,30 +1,25 @@
-/* eslint-disable max-len */
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { compare } from 'bcryptjs';
 import { loginQuery } from '../../queries';
-import { CustomError, loginValidate } from '../../utils';
+import { CustomError, loginValidate, generateToken } from '../../utils';
 
-const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, loginPassword } = req.body;
-
-    try {
-      await loginValidate({ email, loginPassword });
-    } catch (err) {
-      throw new CustomError(400, err.details[0].message);
-    }
-
+    await loginValidate({ email, loginPassword });
     const { count, rows }: any = await loginQuery(email);
     if (!count) {
-      throw new CustomError(401, "email doesn't exist");
+      throw new CustomError(400, "email doesn't exist");
     }
     const hashedPassword = await compare(loginPassword, rows[0].getDataValue('password'));
     if (!hashedPassword) {
-      throw new CustomError(401, 'invalid password');
+      throw new CustomError(400, 'invalid password');
     }
+    await generateToken({ id: rows[0].getDataValue('id'), username: rows[0].getDataValue('name'), role: rows[0].getDataValue('role') });
     res.json({ mag: 'logged in successfully' });
   } catch (err) {
-    res.json({ msg: err });
+    res.status(err.status).json({ msg: err.message });
+    next({ msg: err.message });
   }
 };
 
