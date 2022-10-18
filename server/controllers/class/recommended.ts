a351e6a75faa3d9b460821374d76a9b84bc9175a
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-18 22:12:01 +0300

    edit recommended controller (add try/catch, {data} format ) related to #55

--- server/controllers/class/recommended.ts
+++ server/controllers/class/recommended.ts
@@ -1,9 +1,14 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { recommendedQueries } from '../../queries';
 
-const recommended = async (req: Request, res: Response) => {
-  const { classId } = req.params;
-  res.json(await recommendedQueries(Number(classId)));
+const recommended = async (req: Request, res: Response, next:NextFunction) => {
+  try {
+    const { classId } = req.params;
+    const data:any = await recommendedQueries(Number(classId));
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default recommended;
