bcbb9af8d31b9098776834e8fd2dc34f443440c2
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-18 21:56:31 +0300

    Served the class route close #40

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { userAuth, teacherAuth } from '../middlewares';
 import getStats from '../controllers/class/getStats';
 
 const classRouter = Router();
 
-classRouter.get('/:classId/statistics', getStats);
+classRouter.get('/:classId/statistics', userAuth, teacherAuth, getStats);
 
 export default classRouter;
