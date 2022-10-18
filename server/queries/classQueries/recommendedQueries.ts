bce926b910b0fce530ff008ef6127bafba6ab847
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-18 22:13:18 +0300

    edit recommendedQueries so it will return all attributes related to #55

--- server/queries/classQueries/recommendedQueries.ts
+++ server/queries/classQueries/recommendedQueries.ts
@@ -2,7 +2,6 @@ import { Recommendation } from '../../models';
 
 const recommendedQueries = async (classId: Number) => Recommendation
   .findAll({
-    attributes: ['id', 'material_link', 'description'],
     where: { class_id: classId },
   });
 
