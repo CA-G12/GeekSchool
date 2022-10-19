c6fa1463397a8b95a3c913d6ea3c6727b4990aa7
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-19 10:06:54 +0300

    edit return data form  related to #55

--- server/controllers/class/recommended.ts
+++ server/controllers/class/recommended.ts
@@ -5,7 +5,7 @@ const recommended = async (req: Request, res: Response, next:NextFunction) => {
   try {
     const { classId } = req.params;
     const data:any = await recommendedQueries(Number(classId));
-    res.json({ data });
+    res.json({ msg: 'Recommended data for this class', data });
   } catch (error) {
     next(error);
   }
