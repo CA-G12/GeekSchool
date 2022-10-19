8f13917754774771e37c5b1bdfa011995ce10887
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-19 23:44:58 +0300

    i added the auth middleware related to #30

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,9 +1,9 @@
-import { userAuth, studentAndTeacher } from '../middlewares';
+import { userAuth, studentAndTeacher, teacherAuth } from '../middlewares';
 import { getAnnouncement, getAllStudentHowSubmitTasks } from '../controllers';
 
 const classRouter = require('express').Router();
 
 classRouter.get('/class/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
-classRouter.get('/class/:classId/assignment/:assignmentId/students', getAllStudentHowSubmitTasks);
+classRouter.get('/class/:classId/assignment/:assignmentId/students', teacherAuth, getAllStudentHowSubmitTasks);
 
 export default classRouter;
