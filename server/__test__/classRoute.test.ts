26c3452ab4ea0da733620ba5eed1f4e0d38281ff
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-20 00:01:43 +0300

    test the route related to #30

--- server/__test__/classRoute.test.ts
+++ server/__test__/classRoute.test.ts
@@ -35,4 +35,31 @@ describe('Testing class routes', () => {
         return done();
       });
   });
+
+  test('should NOT return the students who  submitted the assignment without token', (done) => {
+    supertest(app)
+      .get('/api/v1/class/2/assignment/8/students')
+      .expect(401)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.msg).toEqual('Unauthenticated');
+        return done();
+      });
+  });
+
+  test('should  return the students who  submitted the assignment with token', (done) => {
+    supertest(app)
+      .get('/api/v1/class/2/assignment/8/students')
+      .expect(200)
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
+      ])
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.msg).toEqual('getting all student successfully');
+        return done();
+      });
+  });
 });
