b35f18ac5cdb05419bf669c923f03fa2807240d9
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-19 11:43:38 +0300

    Served deleteStudentFromClass route close #42

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,8 +1,9 @@
-import { userAuth, studentAndTeacher } from '../middlewares';
-import { getAnnouncement } from '../controllers';
+import { userAuth, studentAndTeacher, teacherAuth } from '../middlewares';
+import { getAnnouncement, deleteStudentFromARequest } from '../controllers';
 
 const classRouter = require('express').Router();
 
 classRouter.get('/class/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
+classRouter.delete('/class/:classId/student/delete', userAuth, teacherAuth, deleteStudentFromARequest);
 
 export default classRouter;
