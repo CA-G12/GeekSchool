5f64d5ddbd8329e02f135a2096f53bdd1a779e75
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-19 23:06:01 +0300

    Served add new assignment route close #44

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,8 +1,9 @@
-import { userAuth, studentAndTeacher } from '../middlewares';
-import { getAnnouncement } from '../controllers';
+import { userAuth, studentAndTeacher, teacherAuth } from '../middlewares';
+import { getAnnouncement, addNewAssignment } from '../controllers';
 
 const classRouter = require('express').Router();
 
-classRouter.get('/class/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
+classRouter.get('/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
+classRouter.post('/:classId/assignment/add', userAuth, teacherAuth, addNewAssignment);
 
 export default classRouter;
