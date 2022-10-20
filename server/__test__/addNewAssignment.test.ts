ccb018104c9c028b8e73be877eb1e49ed403e30c
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-20 10:44:30 +0300

    Modified the tests to match the chages required close #44

--- server/__test__/addNewAssignment.test.ts
+++ server/__test__/addNewAssignment.test.ts
@@ -11,7 +11,7 @@ afterAll(() => sequelize.close());
 describe('Testing add new assignment route', () => {
   test('Testing add new assignment route: the test should return 201 with the data if the user is logged in and he is a teacher', (done) => {
     supertest(app)
-      .post('/api/v1/class/25/assignment/add')
+      .post('/api/v1/class/25/assignment')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -27,7 +27,7 @@ describe('Testing add new assignment route', () => {
 
   test('Should return the same title, description, and class_id if the user is logged in and he is a teacher', (done) => {
     supertest(app)
-      .post('/api/v1/class/25/assignment/add')
+      .post('/api/v1/class/25/assignment')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -45,7 +45,7 @@ describe('Testing add new assignment route', () => {
 
   test('Should return 401 and Unauthenticated when there is no token or the token is invalid for this route!', (done) => {
     supertest(app)
-      .post('/api/v1/class/25/assignment/add')
+      .post('/api/v1/class/25/assignment')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1Ni.eyJpZCI6MSwibmFtZSI6Ik11c3HMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -61,7 +61,7 @@ describe('Testing add new assignment route', () => {
 
   test('Should return 400 and No title or description or both are given! message when there is no body given for this route!', (done) => {
     supertest(app)
-      .post('/api/v1/class/25/assignment/add')
+      .post('/api/v1/class/25/assignment')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -69,7 +69,7 @@ describe('Testing add new assignment route', () => {
       .expect(400)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body.msg).toBe('No title or description or both are given!');
+        expect(res.body.msg).toBe('Wrong data is inserted!');
         return done();
       });
   });
