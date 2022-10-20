485587ef9595d8f0c1806414a54f4b472b21bf8c
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-20 13:01:21 +0300

    Edited the tests to match the new data structure close #40

--- server/__test__/class.test.ts
+++ server/__test__/class.test.ts
@@ -16,7 +16,7 @@ describe('Testing the statistics router', () => {
   });
   test('Should return 200 status when the user is logged in as a teacher', (done) => {
     supertest(app)
-      .get('/api/v1/class/7/statistics')
+      .get('/api/v1/class/12/statistics')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -39,7 +39,7 @@ describe('Testing the statistics router', () => {
       .expect(200)
       .end((err, res) => {
         if (err) done(err);
-        expect(typeof Number(res.body.data.submitted[0].count)).toBe('number');
+        expect(Array.isArray(res.body.data.submitted)).toBe(true);
         return done();
       });
   });
@@ -54,7 +54,7 @@ describe('Testing the statistics router', () => {
       .expect(200)
       .end((err, res) => {
         if (err) done(err);
-        expect(typeof Number(res.body.data.notSubmitted[0].count)).toBe('number');
+        expect(Array.isArray(res.body.data.notSubmitted)).toBe(true);
         return done();
       });
   });
