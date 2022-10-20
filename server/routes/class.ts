aa4609540a9ba40180215e86284eb4efb0eeca47
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-20 10:36:31 +0300

    create git assignments route related to #54

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,8 +1,9 @@
 import { userAuth, studentAndTeacher } from '../middlewares';
-import { getAnnouncement } from '../controllers';
+import { getAnnouncement, studentAssignment } from '../controllers';
 
 const classRouter = require('express').Router();
 
 classRouter.get('/class/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
+classRouter.get('/class/:classId/assignments/:studentId?', userAuth, studentAndTeacher, studentAssignment);
 
 export default classRouter;
