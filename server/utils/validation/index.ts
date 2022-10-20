b7ea679e35fb9d749ccd21b66bc127d61ead8424
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-20 16:32:56 +0300

    Added a validation for delete a student from a class route close #42

--- server/utils/validation/index.ts
+++ server/utils/validation/index.ts
@@ -1,9 +1,11 @@
 import userValidation from './userValidation';
 import parentValidation from './parentValidation';
 import loginValidate from './loginValidate';
+import deleteStudentFromClassValidation from './deleteStudentFromClassValidation';
 
 export {
   userValidation,
   parentValidation,
   loginValidate,
+  deleteStudentFromClassValidation,
 };
