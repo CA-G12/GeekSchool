8ff0101cd67d50680ec241ccb09dd8d78f072131
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-20 12:54:12 +0300

    fix errors related to #54

--- server/queries/class/teacherAssignmentQuery.ts
+++ server/queries/class/teacherAssignmentQuery.ts
@@ -7,13 +7,10 @@ const teacherAssignmentQuery = async (classId : string) => AssignmentStudent
     nest: false,
     attributes: ['Assignment.id' as 'id', 'Assignment.title' as 'title',
     'Assignment.description' as 'description', 'Assignment.class_id' as 'class_id'],
-    where: {
-    },
     include: [{
       model: Assignment,
       attributes: [],
       where: { class_id: classId },
-
     }],
     group: ['Assignment.id', 'AssignmentStudent.assignment_id'],
 
