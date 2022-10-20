860f33b06e70aaacccf0fbbf03f6cc39ca0c667e
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-20 16:29:09 +0300

    Edited a test to match the new changes close #42

--- server/__test__/deleteStudentClass.test.ts
+++ server/__test__/deleteStudentClass.test.ts
@@ -11,7 +11,7 @@ afterAll(() => sequelize.close());
 describe('Testing delete a student from a class route!', () => {
   test('Testing the success path: the user is a teacher and the user is logged in', (done) => {
     supertest(app)
-      .delete('/api/v1/class/8/student/delete')
+      .delete('/api/v1/class/8/student')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -20,14 +20,14 @@ describe('Testing delete a student from a class route!', () => {
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) done(err);
-        expect(res.body.data.id).toBe(8);
+        expect(res.body.data).not.toBeDefined();
         return done();
       });
   });
 
   test('The function should return 401 status with the Unauthenticated message', (done) => {
     supertest(app)
-      .delete('/api/v1/class/8/student/delete')
+      .delete('/api/v1/class/8/student')
       .send({ studentId: 8 })
       .expect(401)
       .expect('Content-Type', /json/)
@@ -40,7 +40,7 @@ describe('Testing delete a student from a class route!', () => {
 
   test('The function should return 401 status with the Unauthenticated message', (done) => {
     supertest(app)
-      .delete('/api/v1/class/8/student/delete')
+      .delete('/api/v1/class/8/student')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -48,7 +48,7 @@ describe('Testing delete a student from a class route!', () => {
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) done(err);
-        expect(res.body.msg).toBe('The student id is not given!');
+        expect(res.body.msg).toBe('Wrong data is inserted!');
         return done();
       });
   });
