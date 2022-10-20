831151b5143ae157758cac904366b2ca7f393a54
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-20 16:21:03 +0300

    create delete assignment route related to #54

--- server/controllers/class/deleteAssignment.ts
+++ server/controllers/class/deleteAssignment.ts
@@ -5,12 +5,12 @@ import { CustomRequest, CustomError } from '../../utils';
 const deleteAssignment = async (req: CustomRequest, res: Response, next:NextFunction) => {
   try {
     const { user } = req;
-    const { assignmentId } = req.params;
+    const { id } = req.params;
     if (user.role === 'student') {
       next(new CustomError(401, 'Unauthenticated'));
     } else {
-      const data:any = await deleteAssignmentQuery(Number(assignmentId));
-      res.json({ msg: 'Assignment deleted successfully', data });
+      await deleteAssignmentQuery(Number(id));
+      res.json({ msg: 'Assignment deleted successfully' });
     }
   } catch (error) {
     next(error);
