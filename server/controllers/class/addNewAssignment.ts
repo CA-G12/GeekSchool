b810f57cae976cc9c1e065ba4a493afdb4633c91
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-20 10:47:00 +0300

    Added the validation and changed pass the error to the next function close #44

--- server/controllers/class/addNewAssignment.ts
+++ server/controllers/class/addNewAssignment.ts
@@ -1,26 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 
 import { addNewAssignmentQuery } from '../../queries/class';
-import CustomError from '../../utils/CustomError';
+import { CustomError, addNewAssignmentValidation } from '../../utils';
 
 const addNewAssignment = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { classId } = req.params;
+    await addNewAssignmentValidation({
+      classId: req.params.classId, title: req.body.title, description: req.body.description,
+    });
 
-    if (!req.body.title || !req.body.description) {
-      throw new CustomError(400, 'No title or description or both are given!');
-    }
+    const { classId } = req.params;
 
     const { title, description } = req.body;
 
     const assignment = await addNewAssignmentQuery(classId, title, description);
 
-    if (!assignment) throw new CustomError(500, 'Error happen while creating the assignment!');
-
-    res.status(201).json({ message: 'Assignment is created successfully!', data: assignment });
+    res.status(201).json({ msg: 'Assignment is created successfully!', data: assignment });
   } catch (err) {
     if (err.name === 'ValidationError') {
-      throw new CustomError(400, 'Wrong data is inserted!');
+      next(new CustomError(400, 'Wrong data is inserted!'));
     }
     next(err);
   }
