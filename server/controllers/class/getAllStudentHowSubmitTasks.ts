dc01de1e627e999302d633bad9b1a56ef0dabf0a
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-20 23:25:35 +0300

    validate isSubmitted query string value related to #30

--- server/controllers/class/getAllStudentHowSubmitTasks.ts
+++ server/controllers/class/getAllStudentHowSubmitTasks.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import { getStudentTasksQuery } from '../../queries/class';
+import { CustomError } from '../../utils';
 
 const getAllStudentWhoSubmitTasks = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const { assignmentId } = req.params;
     const { isSubmitted } = req.query;
 
+    if (typeof Boolean(isSubmitted) !== 'boolean') {
+      throw new CustomError(422, 'Unprocessable Entity');
+    }
     const data:any = await getStudentTasksQuery(assignmentId, isSubmitted);
     res.json({ msg: 'getting all student successfully', data });
   } catch (error) {
