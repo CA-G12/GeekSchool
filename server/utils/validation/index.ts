d26504a5ebfcabcbb46576baaacfce25eac09582
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-21 19:23:29 +0300

    created putAnswerQuestionvalidation to check if the data is requierd or not . relates #62

--- server/utils/validation/index.ts
+++ server/utils/validation/index.ts
@@ -1,9 +1,11 @@
 import userValidation from './userValidation';
 import parentValidation from './parentValidation';
 import loginValidate from './loginValidate';
+import putAnswerQuestionValidate from './putAnswerQuestionValidate';
 
 export {
   userValidation,
   parentValidation,
   loginValidate,
+  putAnswerQuestionValidate,
 };
