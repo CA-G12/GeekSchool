dba58edf8d8ef005f47ecc6305e7cc739f8bc40b
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-21 22:14:23 +0300

    created announcemant validation. relates #63

--- server/utils/validation/index.ts
+++ server/utils/validation/index.ts
@@ -1,9 +1,11 @@
 import userValidation from './userValidation';
 import parentValidation from './parentValidation';
 import loginValidate from './loginValidate';
+import addAnnouncementValidate from './addAnnouncementValidate';
 
 export {
   userValidation,
   parentValidation,
   loginValidate,
+  addAnnouncementValidate,
 };
