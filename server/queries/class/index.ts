3e12781b5840aa11b8985c50ff1343f86203acba
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-22 01:24:12 +0300

    rename getClassStudent to getClassStudentQuery

--- server/queries/class/index.ts
+++ server/queries/class/index.ts
@@ -1,11 +1,15 @@
 import getAnnouncementQuery from './getAnnouncementQuery';
 import getClassQuestionsQuery from './getClassQuestionsQuery';
 import recommendedQueries from './recommendedQueries';
+import putAssignmentTeacherQuery from './putAssignmentTeacherQuery';
+import putAssignmentStudentQuery from './putAssignmentStudentQuery';
+import getClassStudentsQuery from './getClassStudentsQuery';
 
 export {
-  getClassStudentsQuery,
+  getClassQuestionsQuery,
   getAnnouncementQuery,
   putAssignmentTeacherQuery,
   putAssignmentStudentQuery,
   recommendedQueries,
+  getClassStudentsQuery,
 };
