63c9236fda53827d6912d63a306e8d11ac37f272
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-21 19:20:39 +0300

    created putAnswerQuestion controller function thats update answer for specially question. relates #62

--- server/controllers/class/putAnswerQuestion.ts
+++ server/controllers/class/putAnswerQuestion.ts
@@ -1,15 +1,23 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { putAnswerQuestionQuery } from '../../queries';
-import { CustomError } from '../../utils';
+import { CustomError, CustomRequest, putAnswerQuestionValidate } from '../../utils';
 
-const putAnswerQuestion = async (req: Request, res: Response, next: NextFunction) => {
+const putAnswerQuestion = async (req: CustomRequest, res: Response, next: NextFunction) => {
   try {
+    const { role } = req.user;
     const { classId, questionId } = req.params;
     const { answer } = req.body;
-    const isUpdate = await putAnswerQuestionQuery({ classId, questionId, answer });
-    if (!isUpdate[0]) throw new CustomError(400, 'bad request');
-    res.send({ msg: 'answer was update' });
+
+    if (!(role === 'teacher')) throw new CustomError(401, 'Unauthenticated');
+
+    await putAnswerQuestionValidate({ classId, questionId, answer });
+
+    const data = await putAnswerQuestionQuery({ classId, questionId, answer });
+    if (!data[0]) throw new CustomError(400, 'bad request');
+    res.send({ msg: 'answer was update', answer: data[1][0].getDataValue('answer') });
   } catch (error) {
+    if (error.name === 'ValidationError') next(new CustomError(400, 'answer is required'));
+
     next(error);
   }
 };
