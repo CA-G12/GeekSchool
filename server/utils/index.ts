9e73993d6257589c97fdf765ac372d9d99cbd91c
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-21 22:12:22 +0300

    imported and export announcementInterface and addAnnouncementValidation. relates #63

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,9 +1,9 @@
 import CustomError from './CustomError';
 import {
-  UserRequestInterface, CustomRequest, PayloadInterface, UserTableInterface,
+  UserRequestInterface, CustomRequest, PayloadInterface, UserTableInterface, announcementInterface,
 } from './interfaces';
 import {
-  userValidation, parentValidation, loginValidate,
+  userValidation, parentValidation, loginValidate, addAnnouncementValidate,
 } from './validation';
 import { signToken, verifyToken } from './jwt';
 
@@ -18,4 +18,6 @@ export {
   signToken,
   verifyToken,
   loginValidate,
+  announcementInterface,
+  addAnnouncementValidate,
 };
