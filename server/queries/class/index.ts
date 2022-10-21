512d5708644f9d00593d882408b45d7e5cc43ce1
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-21 22:08:59 +0300

    created addAnnouncementQuery funciton thats post new announcement to database. relates #63

--- server/queries/class/index.ts
+++ server/queries/class/index.ts
@@ -1,9 +1,11 @@
 import getAnnouncementQuery from './getAnnouncementQuery';
 import getClassQuestionsQuery from './getClassQuestionsQuery';
 import recommendedQueries from './recommendedQueries';
+import addAnnouncementQuery from './addAnnouncementQuery';
 
 export {
   getClassQuestionsQuery,
   getAnnouncementQuery,
   recommendedQueries,
+  addAnnouncementQuery,
 };
