7d0f68671c206c7242b018e428ba517f1d2ce7e8
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-21 14:05:51 +0300

    iadded the auth middleware to the update assignment routes

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -2,7 +2,8 @@ import userAuth from './userAuth';
 import teacherAuth from './teacherAuth';
 import parentAuth from './parentAuth';
 import studentAndTeacher from './studentAndTeacher';
+import studentAuth from './studentAuth';
 
 export {
-  userAuth, teacherAuth, parentAuth, studentAndTeacher,
+  userAuth, teacherAuth, parentAuth, studentAndTeacher, studentAuth,
 };
--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,4 +1,6 @@
-import { userAuth, studentAndTeacher /* teacherAuth */ } from '../middlewares';
+import {
+  userAuth, studentAndTeacher, teacherAuth, studentAuth,
+} from '../middlewares';
 import {
   getAnnouncement, putAssignmentTeacher, putAssignmentStudent, getClassQuestions, recommended,
 } from '../controllers';
@@ -8,8 +10,8 @@ const classRouter = require('express').Router();
 classRouter.get('/class/:classId/recommended', userAuth, studentAndTeacher, recommended);
 
 classRouter.get('/class/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
-classRouter.put('/class/:classId/assignment/teachers/:assignmentId', putAssignmentTeacher);
-classRouter.put('/class/:classId/assignment/students/:assignmentId', putAssignmentStudent);
+classRouter.put('/class/:classId/assignment/teachers/:assignmentId', userAuth, teacherAuth, putAssignmentTeacher);
+classRouter.put('/class/:classId/assignment/students/:assignmentId', userAuth, studentAuth, putAssignmentStudent);
 
 classRouter.get('/class/:classId/questions', userAuth, studentAndTeacher, getClassQuestions);
 
