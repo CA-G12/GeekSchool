81288b6a574a3ad795f3aa7bac79130396ce328a
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-22 14:19:54 +0300

    create post test validation related to #58

--- server/utils/validation/index.ts
+++ server/utils/validation/index.ts
@@ -1,9 +1,11 @@
 import userValidation from './userValidation';
 import parentValidation from './parentValidation';
 import loginValidate from './loginValidate';
+import testValidation from './testValidation';
 
 export {
   userValidation,
   parentValidation,
   loginValidate,
+  testValidation,
 };
