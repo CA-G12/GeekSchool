69346a46a98c3506dceccb8dbb30a859b41f3f31
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-22 13:27:55 +0300

    create post recommendation controller related to #57

--- server/controllers/class/index.ts
+++ server/controllers/class/index.ts
@@ -2,10 +2,12 @@ import getClassStudents from './getClassStudents';
 import getAnnouncement from './getAnnouncement';
 import recommended from './recommended';
 import getClassQuestions from './getClassQuestions';
+import postRecommendation from './postRecommendation';
 
 export {
   getClassStudents,
   getAnnouncement,
   getClassQuestions,
   recommended,
+  postRecommendation,
 };
