eae26bb2b8204f693abca78ca290f6cb7c8d5b5e
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-22 13:23:50 +0300

    create recommendation interface related to #57

--- server/utils/interfaces/index.ts
+++ server/utils/interfaces/index.ts
@@ -4,6 +4,7 @@ import PayloadInterface from './payloadInterface';
 import UserValInterface from './userValInterface';
 import UserTableInterface from './userTabelInterface';
 import loginValidateInterface from './auth';
+import recommendationInterface from './recommendationInterface';
 
 export {
   CustomRequest,
@@ -12,4 +13,5 @@ export {
   UserValInterface,
   UserTableInterface,
   loginValidateInterface,
+  recommendationInterface,
 };
