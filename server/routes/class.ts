8f85431dc443110aeb01cb350cb1519a769ebb6c
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-22 14:53:12 +0300

    Resolved conflict close #40

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { userAuth, teacherAuth, studentAndTeacher } from '../middlewares';
 import {
-  getStats, getAnnouncement, getClassQuestions, recommended,
+  getStats, getAnnouncement, getClassQuestions, recommended, getClassStudents,
 } from '../controllers';
 
 const classRouter = Router();
@@ -10,5 +10,6 @@ classRouter.get('/:classId/statistics', userAuth, teacherAuth, getStats);
 classRouter.get('/:classId/announcement', userAuth, studentAndTeacher, getAnnouncement);
 classRouter.get('/:classId/recommended', userAuth, studentAndTeacher, recommended);
 classRouter.get('/:classId/questions', userAuth, studentAndTeacher, getClassQuestions);
+classRouter.get('/:classId/students', userAuth, studentAndTeacher, getClassStudents);
 
 export default classRouter;
