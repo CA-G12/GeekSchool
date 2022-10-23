50ddb24bd3b81e8c0fbb80058469b96022e1d867
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-23 14:55:25 +0300

    imported recommendedCard, questionsCard, and feedbackCard and exported this. relates #85

---
+++ client/src/components/index.tsx
@@ -0,0 +1,5 @@
+import RecommendedCard from "./RecommendedCard";
+import FeedbackCard from "./FeedbackCard";
+import QuestionsCard from "./QuestionsCard";
+
+export { RecommendedCard, FeedbackCard, QuestionsCard };
