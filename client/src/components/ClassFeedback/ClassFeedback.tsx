cb7b0da9d54a49c54a1c7d2e4eda67b9881b6b91
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-23 23:26:31 +0300

    commiting a test file

--- client/src/components/ClassFeedback/ClassFeedback.tsx
+++ client/src/components/ClassFeedback/ClassFeedback.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import { Button, Form, Input } from 'antd';
+import React from "react";
+import { Button, Form, Input } from "antd";
 
 const ClassFeedback: React.FC = () => {
   const onFinish = (values: any) => {
-    console.log('Success:', values);
+    console.log("Success:", values);
   };
 
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
 
-    return (
-      <section className="class-feedback">
-        <h1>Feedback</h1>
-        <section className='feedback-inner'>
-          <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+  return (
+    <section className="class-feedback">
+      <h1>Feedback</h1>
+      <section className="feedback-inner">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="feedback"
+            rules={[{ required: true, message: "Please type your feedback!" }]}
           >
-            <Form.Item
-              name="feedback"
-              rules={[{ required: true, message: 'Please type your feedback!' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
             </Button>
-            </Form.Item>
-          </Form>
-        </section>
+          </Form.Item>
+        </Form>
       </section>
-    );
+    </section>
+  );
 };
 
 export default ClassFeedback;
