b1bd2b2a2ad26292ac6bf4ca27591c4bd2826fdf
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-23 13:19:36 +0300

    init related to #88

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { UserAuthProvider } from "./context/AuthContext";
+import AssignmentModal from './components/AssignmentModal'
 
 const App = () => (
   <UserAuthProvider>
     <div className="App">hi</div>
+    <AssignmentModal />
   </UserAuthProvider>
 );
 
---
+++ client/src/components/AssignmentModal.tsx
@@ -0,0 +1,95 @@
+// import { render } from '@testing-library/react';
+import { Button, Modal, Form, Input } from 'antd';
+import { useState } from 'react';
+import {PlusOutlined }from '@ant-design/icons';
+import './AssignmentModal.css'
+
+
+
+const AssignmentModal = () => {
+
+    const [visible, setVisible] = useState<boolean>(false);
+
+    const showModal = () => setVisible(true);
+
+    const handleCancel = () => {
+        console.log('hello handleCancel');
+        setVisible(false);
+    }
+
+
+    return (
+        <div>
+            <Button type="primary" onClick={() => showModal()}><PlusOutlined /> Add</Button>
+            <Modal
+                cancelButtonProps={{ style: { display: 'none' } }}
+                okButtonProps={{ style: { display: 'none' } }}
+
+                visible={visible}
+                onCancel={handleCancel}
+                width="60%"
+
+            >
+
+                <Form >
+                    <br />
+
+                    <Form.Item
+                        label="Assignment title"
+                        style={{ width: "80%" }}
+                        name="title"
+                        rules={[
+                            {
+
+                                required: true,
+                                message: "Assignment title is required",
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Assignment title" width="100%" />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Assignment description: "
+                        style={{ width: "100%" }}
+                        name="title"
+                        rules={[
+                            {
+
+                                required: true,
+                                message: "Assignment details is required",
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Assignment description" width="60%" height="100%" />
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{ span: 8, offset: 4 }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+
+                </Form>
+
+            </Modal>
+        </div >
+
+    );
+
+
+
+}
+
+
+
+
+export default AssignmentModal;
+
+
+
+
+
+
