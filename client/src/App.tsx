37501f7e699710181de740e922363f08ce5945f6
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-23 22:11:00 +0300

    adding the question component to the class page related to #37

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { ConfigProvider } from "antd";
 
 import "./i18n/config.js";
 import StatsDummy from "./components/StatsDummy/Dummy";
+import Question from './components/Class/Questions'
 import { UserAuthProvider } from "./context/AuthContext";
 import Class from "./components/Class";
 import "antd/dist/antd.variable.min.css";
@@ -37,7 +38,7 @@ const router = createBrowserRouter([
       },
       {
         path: "questions",
-        element: <h1>Questions</h1>,
+        element: <Question />,
       },
       {
         path: "feedback",
