0c4f9e6ba4a35e100b3980a1d9730e26eb8ac5dc
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-24 11:43:32 +0300

    create teacher/student card related to #95

---
+++ client/src/components/assignmentCard/index.tsx
@@ -0,0 +1,8 @@
+import TeacherAssignmentCard from "./TeacherAssignmentCard";
+import StudentAssignmentCard from "./StudentAssignmentCard";
+
+
+export {
+    TeacherAssignmentCard,
+    StudentAssignmentCard
+}
