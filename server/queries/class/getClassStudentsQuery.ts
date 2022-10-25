5f781462072a0016651cd04d38d88158af0c4b98
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-25 14:24:39 +0300

    update returned data

--- server/queries/class/getClassStudentsQuery.ts
+++ server/queries/class/getClassStudentsQuery.ts
@@ -6,7 +6,6 @@ const getClassStudentsQuery = (classId: string) => ClassStudent.findAll(
   {
     raw: true,
     attributes: ['Student.User.name' as 'name', 'Student.User.mobile' as 'MobileNum', 'Student.Parent.User.img' as 'img'],
-    // attributes: { exclude: ['id', 'createdAt', 'updatedAt', 'class_id'] },
     where: { class_id: classId },
     include: [{
       model: Student,
@@ -14,10 +13,6 @@ const getClassStudentsQuery = (classId: string) => ClassStudent.findAll(
       include: [{
         model: User,
         attributes: [],
-
-        // attributes: {
-        //   exclude: ['createdAt', 'updatedAt', 'password'],
-        // },
       },
       {
         model: Parent,
