a08805d38cae9e1169e5ddeed948097e9cc38d02
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-25 18:52:15 +0300

    Finshed the assignment page close #100

--- client/src/components/Class/Assignments/Assignments.tsx
+++ client/src/components/Class/Assignments/Assignments.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Dropdown, Space, Button, Menu, message, MenuProps, Input } from "antd";
 import {
   PlusOutlined,
@@ -6,24 +6,30 @@ import {
   FileProtectOutlined,
 } from "@ant-design/icons";
 import axios from "axios";
-import { StudentAssignmentCard } from "../cards";
+import { useUserData } from "../../../context/AuthContext";
+import AddTest from "../../ClassTests/AddTest/AddTest";
+import { StudentAssignmentCard, TeacherAssignmentCard } from "../cards";
 import "./Assignments.css";
 
 const { Search } = Input;
 
 const Assignments: React.FC = () => {
   const [assignments, setAssignments] = useState<Array<object>>([]);
-  const [, setAddTest] = useState<boolean>(false);
+  const [addTest, setAddTest] = useState<boolean>(false);
   const [, setAddAssignment] = useState<boolean>(false);
+  const submittedOrNotNum = useRef({
+    submitted: 0,
+    notSubmitted: 0,
+  });
 
-  // const role = 'teacher';
   const classId = 5; // ? This will be passed when the user clicks on some class.
+  const { role } = useUserData;
   const source = axios.CancelToken.source();
 
   // ? The search function.
   const onSearch = (value: string) =>
-    setAddAssignment((prevValue: any) =>
-      prevValue.filter((object: any) => object.title.contains(value))
+    setAssignments((prevValue: any) =>
+      prevValue.filter((object: any) => object.title.includes(value))
     );
 
   // ? Button events
@@ -58,8 +64,15 @@ const Assignments: React.FC = () => {
     const fetchAssignmentsData = async () => {
       const data = await axios.get(`/api/v1/class/${classId}/assignments`);
 
-      console.log(data.data.data);
-      setAssignments(data.data.data);
+      if (data.data.data.count) {
+        setAssignments(data.data.data.rows);
+        data.data.data.rows.forEach((assignment: any) => {
+          if (assignment.is_submitted === true) submittedOrNotNum.current.submitted += 1;
+          else submittedOrNotNum.current.notSubmitted += 1;
+        });
+      } else {
+        setAssignments(data.data.data);
+      }
     };
 
     fetchAssignmentsData();
@@ -68,34 +81,52 @@ const Assignments: React.FC = () => {
   }, []);
 
   return (
-    <main className="class-assignment">
-      <h1 className="assignment-title">التكليفات</h1>
-      <section className="assignment-add-search">
-        <Search
-          placeholder="ابحث عن تكليف"
-          className="search"
-          onSearch={onSearch}
-          enterButton
-        />
-        <Dropdown overlay={menu} className="dropdown">
-          <Button className="dropdown-button">
-            <Space>
-              <PlusOutlined className="plus-icon" />
-              إضافة
-            </Space>
-          </Button>
-        </Dropdown>
-      </section>
-      <section className="assignments-box">
-        {assignments.map((assignment: any) => (
-          <StudentAssignmentCard
-            title={assignment.title}
-            createdAt={assignment.createdAt}
-            description={assignment.description}
+    <>
+    {addTest && <AddTest setValue={setAddTest} />}
+        <main className="class-assignment">
+        <h1 className="assignment-title">التكليفات</h1>
+        <section className="assignment-add-search">
+          <Search
+            placeholder="ابحث عن تكليف"
+            className="search"
+            onSearch={onSearch}
+            enterButton
           />
-        ))}
-      </section>
-    </main>
+          <Dropdown overlay={menu} className="dropdown">
+            <Button className="dropdown-button">
+              <Space>
+                <PlusOutlined className="plus-icon" />
+                إضافة
+              </Space>
+            </Button>
+          </Dropdown>
+        </section>
+        {
+          role === 'student' && <section className="assignments-box">
+            {assignments.map((assignment: any) => (
+              <StudentAssignmentCard
+                title={assignment.title}
+                createdAt={assignment.createdAt}
+                description={assignment.description}
+              />
+            ))}
+          </section>
+        }
+        {
+          role === 'teacher' && <section className="assignment-box">
+              {assignments.map((assignment: any) => (
+                <TeacherAssignmentCard
+                  title={assignment.title}
+                  createdAt={assignment.createdAt}
+                  description={assignment.description}
+                  submitted={submittedOrNotNum.current.submitted}
+                  notSubmitted={submittedOrNotNum.current.notSubmitted}
+              />
+              ))}
+            </section>
+        }
+      </main>
+    </>
   );
 };
 
