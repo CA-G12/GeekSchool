88a34fde0352f15fe240fffe9a35851f534dccb1
Bas-sheikh <basilelshakhe@gmail.com>
2022-10-26 20:54:11 +0300

    created putStudentHealth controller function. relates #109

--- server/controllers/student/putStudentHealth.ts
+++ server/controllers/student/putStudentHealth.ts
@@ -6,7 +6,7 @@ const putStudentHealth = async (req: Request, res: Response, next: NextFunction)
     const { studentId } = req.params;
     const { body } = req;
     const data = await putStudentHealthQuery({ studentId, body });
-    res.json({ data: data[1][0] });
+    res.json({ data: data[1][0], msg: 'Health was updated' });
   } catch (error) {
     next(error);
   }
