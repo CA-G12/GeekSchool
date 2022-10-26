c17eae61ac594b7e2c17cea1314ae9b1cd25aff0
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-26 13:15:27 +0300

    resolve conflicts related to #104

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,5 +2,4 @@ import SignUpPage from "./signUp";
 import LoginPage from "./login";
 import RecommendedPage from "./recommended/RecommendedPage";
 
-
 export { SignUpPage, LoginPage, RecommendedPage };
