f2171eebdfa5b35454fb20acc2105896fe78cbfb
Mustafa Salem <mustafaissasalem@gmail.com>
2022-10-27 21:59:19 +0300

    Added the tests for getStudentClasses route close #117

---
+++ server/__test__/getStudentClasses.test.ts
@@ -0,0 +1,81 @@
+import supertest from 'supertest';
+import { sequelize } from '../models';
+import buildSeed from '../database/seed';
+import app from '../app';
+
+jest.setTimeout(20000);
+beforeAll(() => buildSeed());
+afterAll(() => sequelize.close());
+
+describe('Testing get the students for a specific teacher route.', () => {
+  test('Testing the success path, when the user is logged in and the role of him is teacher. The statusCode should be 200.', (done) => {
+    supertest(app)
+      .get('/api/v1/student/5/classes')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsIm5hbWUiOiJKb2huIERvZSIsImlkIjoxNTE2MjM5MDIyfQ.ivV7KczMBPLI6JBiY7oAXlcfPuaTVNtd71aTrtgZa8A',
+      ])
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(200);
+        return done();
+      });
+  });
+
+  test('Testing the success path, when the user is logged in and the role of him is teacher', (done) => {
+    supertest(app)
+      .get('/api/v1/student/5/classes')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsIm5hbWUiOiJKb2huIERvZSIsImlkIjoxNTE2MjM5MDIyfQ.ivV7KczMBPLI6JBiY7oAXlcfPuaTVNtd71aTrtgZa8A',
+      ])
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(Array.isArray(res.body.data)).toBe(true);
+        return done();
+      });
+  });
+
+  test('Testing the failure path, when the user is logged in and the role of him is not teacher, it should return Unauthenticated', (done) => {
+    supertest(app)
+      .get('/api/v1/student/5/classes')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEiLCJyb2xlIjoicGFyZW50IiwiaWF0IjoxNjY2ODU2OTg5fQ.zRPQHH51kwdsFlF4wDZP1kT7RCRmchw4YtflOFCWtYc',
+      ])
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(401);
+        expect(res.body.msg).toBe('Unauthenticated!');
+        return done();
+      });
+  });
+
+  test('Testing the failure path, when the user is logged in, it should return Unauthenticated', (done) => {
+    supertest(app)
+      .get('/api/v1/student/5/classes')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(401);
+        expect(res.body.msg).toBe('Unauthenticated');
+        return done();
+      });
+  });
+
+  test('Testing the fail path, when the user is logged in and the role of him is teacher but the params is not valid.', (done) => {
+    supertest(app)
+      .get('/api/v1/student/5ttt/classes')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsIm5hbWUiOiJKb2huIERvZSIsImlkIjoxNTE2MjM5MDIyfQ.ivV7KczMBPLI6JBiY7oAXlcfPuaTVNtd71aTrtgZa8A',
+      ])
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(404);
+        expect(res.body.msg).toBe('The user id you searched with have no classes!');
+        return done();
+      });
+  });
+});
