3695c48142d5c755ee49de92cc346d27b0c4b99c
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-28 13:52:57 +0200

    testing the route related to #123

--- server/__test__/profile.test.ts
+++ server/__test__/profile.test.ts
@@ -41,4 +41,34 @@ describe('Testing the statistics router', () => {
         return done();
       });
   });
+
+  test('Should return all classes for a specific teacher', (done) => {
+    supertest(app)
+      .get('/api/v1/profile/teacher/7/classes')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
+      ])
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body.msg).toBe('getting all student successfully');
+        return done();
+      });
+  });
+
+  test('Should return 401 because the token is invalid', (done) => {
+    supertest(app)
+      .get('/api/v1/profile/teacher/7/classes')
+      .set('Cookie', [
+        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
+      ])
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body.msg).toBe('Unauthenticated');
+        return done();
+      });
+  });
 });
--- server/queries/profile/getTeacherClassesQuery.ts
+++ server/queries/profile/getTeacherClassesQuery.ts
@@ -1,17 +1,12 @@
 import { Class } from '../../models';
 
-const getTeacherClassesQuery = (teacherId: string) => {
-  console.log(teacherId);
-
-  return (
-    Class.findAll({
-      raw: true,
-      where: {
-        teacher_id: teacherId,
-      },
-    //   attributes: ['user_id', 'User.name' as'name', 'User.img' as 'img', 'parent_id'],,
-    })
-  );
-};
+const getTeacherClassesQuery = (teacherId: string) => (
+  Class.findAll({
+    raw: true,
+    where: {
+      teacher_id: teacherId,
+    },
+  })
+);
 
 export default getTeacherClassesQuery;
