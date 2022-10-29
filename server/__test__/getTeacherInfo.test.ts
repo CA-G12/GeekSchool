d3eac950645b54257f3941e70440099cd4843904
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-29 20:44:11 +0300

    edit get teacherIno tests  related to #112

--- server/__test__/getTeacherInfo.test.ts
+++ server/__test__/getTeacherInfo.test.ts
@@ -11,7 +11,7 @@ afterAll(() => sequelize.close());
 describe('Testing get teacherInfo route', () => {
   test('should return not auth because students are not allowed', (done) => {
     supertest(app)
-      .get('/api/v1/teacher/1')
+      .get('/api/v1/teacher/info')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsIm5hbWUiOiJKb2huIERvZSIsImlkIjoxNTE2MjM5MDIyfQ.ivV7KczMBPLI6JBiY7oAXlcfPuaTVNtd71aTrtgZa8A',
       ])
@@ -26,7 +26,7 @@ describe('Testing get teacherInfo route', () => {
 
   test('should return the info for this teacher', (done) => {
     supertest(app)
-      .get('/api/v1/teacher/1')
+      .get('/api/v1/teacher/info')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ik11c3RhZmEgU2FsZW0iLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTY2NjExNjM4NH0.sr7oT_2dHMdTWfBKZEC7pa4VOZnlN9vM9y8P1UnsTa8',
       ])
@@ -42,7 +42,7 @@ describe('Testing get teacherInfo route', () => {
 
   test('should not return info because this is not auth', (done) => {
     supertest(app)
-      .get('/api/v1/teacher/1')
+      .get('/api/v1/teacher/info')
       .expect(401)
       .expect('Content-Type', /json/)
       .end((err, res) => {
