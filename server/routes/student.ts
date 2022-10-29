86d466cb5e8dea85c809ebc7fc13352a516a61d8
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-10-30 01:35:25 +0200

    create get studentTests route related to #136

--- server/routes/student.ts
+++ server/routes/student.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { getStudentClasses } from '../controllers';
+import { getStudentClasses, getStudentTests } from '../controllers';
 import { userAuth, studentAuth } from '../middlewares';
 
 const studentRouter = Router();
 
 studentRouter.get('/classes', userAuth, studentAuth, getStudentClasses);
+studentRouter.get('/tests', userAuth, studentAuth, getStudentTests);
 
 export default studentRouter;
