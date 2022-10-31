f24f429be7596390f28ebdb86a252293f328ae12
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-10-31 17:16:30 +0200

    update health middleware

--- server/middlewares/healthMiddleware.ts
+++ server/middlewares/healthMiddleware.ts
@@ -4,14 +4,14 @@ import { CustomError } from '../utils';
 
 const healthMiddleware = async (req: any, res: Response, next: NextFunction) => {
   try {
-    const { id } = req.user;
+    const { id, role } = req.user;
     const { studentId } = req.params;
     const data = await studentParentRelationQuery(id, studentId);
     const isRelated = data[0]?.getDataValue('id');
 
     if (!isRelated) {
       throw new CustomError(401, 'Unauthenticated');
-    } else {
+    } else if (role === 'student' || role === 'parent' || role === 'teacher') {
       next();
     }
   } catch (error) {
