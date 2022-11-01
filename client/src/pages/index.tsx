bb2cb355e7d1b38f8ece0642f62222b5d1fa52d7
Mustafa Salem <mustafaissasalem@gmail.com>
2022-11-01 20:23:01 +0200

    Connecting the different files in idex files close #142

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import SignUpPage from "./signUp";
 import LoginPage from "./login";
 import ProfilePage from "./profile";
+import ParentProfile from "./ParentProfile/ParentProfile";
 
-export { SignUpPage, LoginPage, ProfilePage };
+export { SignUpPage, LoginPage, ProfilePage, ParentProfile };
