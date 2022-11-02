afe7babb17aabda3b0163e09863b3b5f233773d8
Bas-sheikh <basilelshakhe@gmail.com>
2022-11-02 20:44:07 +0200

    created healthCard compnent and styled it thats compnents for handled health type status. relates #147

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,8 +1,15 @@
 import RecommendedCard from "./RecommendedCard";
 import FeedbackCard from "./FeedbackCard";
 import QuestionsCard from "./QuestionsCard";
+import HealthCard from "./healthProfile/HealthProfile";
 import { StudentSignUp, ParentSignUp, TeacherSignUp } from "./signUpForm";
 
-export { RecommendedCard, FeedbackCard, QuestionsCard };
-
-export { StudentSignUp, ParentSignUp, TeacherSignUp };
+export {
+  RecommendedCard,
+  FeedbackCard,
+  QuestionsCard,
+  HealthCard,
+  StudentSignUp,
+  ParentSignUp,
+  TeacherSignUp,
+};
