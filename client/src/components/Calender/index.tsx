5c68d902a08884acfa49870712bc94935e200f54
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-11-02 06:11:42 +0200

    create test calender related to #1444

--- client/src/components/Calender/index.tsx
+++ client/src/components/Calender/index.tsx
@@ -1,71 +1,57 @@
-// import type { BadgeProps } from "antd";
-// import type { Moment } from "moment";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { BadgeProps } from "antd";
 import { Badge, Calendar } from "antd";
 import type { Moment } from "moment";
-import moment from "moment";
+import moment from 'moment';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-// import axios from "axios";
+type testsType = {
+  title: string;
+  id: number;
+  date: string;
+};
 
-// type testsType = {
-//   title: string;
-//   id:number;
-//   date: Date;
-// };
-
-// const fakeData:testsType  = {
-//   title: "test",
-//   id : 1,
-//   date : '17 2022 00:16:25 GMT+0200',
-// }
 
 const Calender: React.FC = () => {
-  // const source = axios.CancelToken.source();
-  // const [tests, setTest] = useState<Array<testsType>>([fakeData]);
-  const getListData = (value: Moment) => {
-    // const date = moment("2014-02-27T10:00:00").format('DD-MM-YYYY');
-    let listData;
+  const source = axios.CancelToken.source();
+  const [tests, setTest] = useState<Array<any>>([]);
 
-    switch (value.date()) {
-      case 8:
-        listData = [{ type: "success", content: "امتحان علوم" }];
-        break;
-      case 10:
-        listData = [
-          { type: "warning", content: "This is warning event." },
-          { type: "success", content: "This is usual event." },
-          { type: "error", content: "This is error event." },
-        ];
-        break;
-      case 15:
-        listData = [
-          { type: "warning", content: "This is warning event" },
-          { type: "success", content: "This is very long usual event。。...." },
-          { type: "error", content: "This is error event 1." },
-        ];
-        break;
-      default:
+  const { studentId } = useParams();
+  useEffect(() => {
+    const fetchTests = async () => {
+      const { data } = await axios.get(
+        `/api/v1/student/${studentId}/tests/`
+      );
+      setTest(data.data);
+    };
+    fetchTests();
+    return () => source.cancel();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+
+
+  const getListData = (value: Moment) => {
+    let listData;
+    tests.map((ele: testsType) => {
+      const newDate = moment(ele.date);
+      if (newDate.isSame(value, 'month') && newDate.isSame(value, 'year') && newDate.isSame(value, 'day')) {
+        listData = [{ type: "success", content: ele.title }];
+      }
+      return null;
     }
+    )
     return listData || [];
   };
 
   const dateCellRender = (value: Moment) => {
-    console.log("dddddddddddddddddd", value);
-
-    const d = new Date("2019/06/01");
-
-    // const myMomentObject = moment(d).format("MMMM d, YYYY");
-    const today = moment(d);
-    console.log(today.date(),'gggggggggggggggggggggggggggggggggg');
-    // const myMomentObject = today.format();
-    // const myMomentObject = moment("05/27/2022", 'YYYY-MM-DD');
-    // console.log("ssssssssssssssssss", myMomentObject.date());
     const listData = getListData(value);
 
     return (
       <ul className="events">
-        {listData.map((item) => (
+        {listData.map((item: any) => (
           <li key={item.content}>
             <Badge
               status={item.type as BadgeProps["status"]}
@@ -77,23 +63,10 @@ const Calender: React.FC = () => {
     );
   };
 
-  console.log("eeeeeee");
-  // useEffect(() => {
-  //   const fetchTests = async () => {
-  //     const { data } = await axios.get(
-  //       `/api/v1/student/1/tests`
-  //     );
-  //     setTest(data);
-
-  //   };
-  //   fetchTests();
-  //   return () => source.cancel();
-  // }, []);
 
   return (
     <Calendar
       dateCellRender={dateCellRender}
-      onSelect={(date) => console.log("selected", date)}
     />
   );
 };
