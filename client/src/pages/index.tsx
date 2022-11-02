846fc3d7081313935ac79253bc0f9b51307d5120
Bas-sheikh <basilelshakhe@gmail.com>
2022-11-02 21:00:18 +0200

    created healthProfile page. and styled this. relates #147

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import SignUpPage from "./signUp";
 import LoginPage from "./login";
 import ProfilePage from "./profile";
+import HealthProfilePage from "./healthProfile";
 
-export { SignUpPage, LoginPage, ProfilePage };
+export { SignUpPage, LoginPage, ProfilePage, HealthProfilePage };
