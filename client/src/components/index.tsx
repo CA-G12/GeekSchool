bc61d4e50e7ae0dc763ad967b3fd6e3cbc4a18e3
Bas-sheikh <basilelshakhe@gmail.com>
2022-11-06 00:02:54 +0200

    resolved conflict . relates #163

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,14 +2,9 @@ import RecommendedCard from "./RecommendedCard";
 import FeedbackCard from "./FeedbackCard";
 import QuestionsCard from "./QuestionsCard";
 import HealthCard from "./healthProfile/HealthProfile";
-import {
-  StudentSignUp,
-  ParentSignUp,
-  TeacherSignUp,
-} from "./signUpForm";
+import { StudentSignUp, ParentSignUp, TeacherSignUp } from "./signUpForm";
 
 import TeacherSchedule from "./TeacherSchedule";
-
 
 export {
   RecommendedCard,
