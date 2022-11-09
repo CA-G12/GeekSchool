607547c763cb930df5f7579fad09ec0ede7085be
mahmoud <mahmoudhammad3009@gmail.com>
2022-11-10 01:16:54 +0200

    fix bugs

--- server/controllers/student/getStudentInfo.ts
+++ server/controllers/student/getStudentInfo.ts
@@ -4,7 +4,6 @@ import { getStudentInfoQuery } from '../../queries';
 const getStudentInfo = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { studentId } = req.params;
-    console.log('000000000000', studentId);
     
     const data = await getStudentInfoQuery(studentId);
     console.log('222222', data);
