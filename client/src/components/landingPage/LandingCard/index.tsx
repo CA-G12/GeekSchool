4166ded0a0ef548fccf8b7c35f108d5afa911e8b
Mahmoud Hammad <67611935+mahmoudhammad309@users.noreply.github.com>
2022-11-15 00:23:30 +0200

    Revert "Class style"

--- client/src/components/landingPage/LandingCard/index.tsx
+++ client/src/components/landingPage/LandingCard/index.tsx
@@ -16,7 +16,7 @@ const LandingCard = () => (
           <div>
             <h2>محمود حماد</h2>
             <p>طالب اعدادي</p>
-            <Rate disabled defaultValue={5} value={5} />
+            <Rate disabled defaultValue={5} value={1} />
           </div>
         </div>
         <div className="hr" />
@@ -32,7 +32,7 @@ const LandingCard = () => (
           <div>
             <h2> باسل الشيخ</h2>
             <p>طالب ثانوية</p>
-            <Rate disabled defaultValue={5} value={1} />
+            <Rate disabled defaultValue={5} value={3} />
           </div>
         </div>
         <div className="hr" />
