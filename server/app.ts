f861e83a051b91d8135a6e6c4a1160f35163ddfa
mahmoud <mahmoudhammad3009@gmail.com>
2022-11-16 15:46:17 +0200

    fix the endpoint

--- server/app.ts
+++ server/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.disable('x-powered-by');
 
-app.use('/api/v1/', router);
+app.use('/api/v1', router);
 
 app.use(notFound);
 app.use(serverError);
